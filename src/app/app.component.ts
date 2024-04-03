249c3eebc7ecace1e48bc7b36c1f08becde72ed8
Damian Łączyński <damian.laczynski.edu@gmail.com>
2024-04-03 18:15:07 +0200

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template:`<h1>Hello World</h1>`
 })
 export class AppComponent {
-  title = 'schedule-app';
+
 }
